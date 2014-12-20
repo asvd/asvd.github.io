80e56800a3f791d4fb0c5ed1a051bf6e578e8812
Dmitry Prokashev <heliosframework@gmail.com>
2014-12-20 21:53:36 +0100

    u

--- squeeze/test8/squeeze.js
+++ squeeze/test8/squeeze.js
@@ -1949,7 +1949,7 @@ function (exports) {
                     height: intSize,
                     backgroundSize:
                         util.px(sideSize) + ' ' +
-                        util.px(coordinates[i].size),
+                        util.px(Math.floor(coordinates[i].size)),
                     backgroundPosition: bgSideOffset + ' ' + bgOffset
                 });
             } else {
@@ -1957,7 +1957,7 @@ function (exports) {
                     left : floorCoord,
                     width : intSize,
                     backgroundSize:
-                        util.px(coordinates[i].size) + ' ' +
+                        util.px(Math.floor(coordinates[i].size)) + ' ' +
                         util.px(sideSize),
                     backgroundPosition: bgOffset + ' '+bgSideOffset
                 });
