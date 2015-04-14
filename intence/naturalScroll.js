cdab13f5ddd004d839d5d29aa1f58dc0114ec86c
Dmitry Prokashev <heliosframework@gmail.com>
2015-04-14 20:09:27 +0200

    framedrop

--- intence/naturalScroll.js
+++ intence/naturalScroll.js
@@ -1,1 +1,142 @@
-!function(e,n){"function"==typeof define&&define.amd?define(["exports"],n):n("undefined"!=typeof exports?exports:e.naturalScroll={})}(this,function(e){var n=[[],[]],f="scrollTop",r="scrollLeft",o=function(e){var o=e?f:r,t=n[e?0:1];return function(e,n,f){e=e.scroller||e,f=f||600;for(var r,l,i=0,a=e[o],c=0,s=0;i<t.length;i++)r=t[i].e==e?t[i]:r;r?(c=r.f[1],s=r.f[2]):t.push(r={e:e});var u=r.l=Math.floor(f/20),p=u*u,d=p*u,v=a-n;r.f=[a,c,s,-(9*s*p+(36*c-9*s)*u-36*c+60*v)/(d-u),6*(6*s*p+(32*c-6*s)*u-32*c+60*v)/u/(d+2*p-u-2),-60*(s*p+(6*c-s)*u-6*c+12*v)/u/(p*p+5*(d+p-u)-6)],r.i||((l=function(n){if(e[o]=(n=r.f)[0],--r.l)for(i=4;i+1;)n[i]+=n[i--+1];else clearInterval(r.i),r.i=n[1]=n[2]=0})(),r.i=setInterval(l,20))}};e[f]=o(e[r]=o())});
+/**
+ * @fileoverview naturalScroll - scrolls an viewport naturally
+ * @version 0.1.0
+ * 
+ * @license MIT, see http://github.com/asvd/naturalScroll
+ * @copyright 2015 asvd <heliosframework@gmail.com> 
+ */
+
+
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['exports'], factory);
+    } else if (typeof exports !== 'undefined') {
+        factory(exports);
+    } else {
+        factory((root.naturalScroll = {}));
+    }
+}(this, function (exports) {
+    var allAnimations = [
+        [],  // vertical scrolling animations, one for an element
+        []   // horizontal animations
+    ];
+
+    // for better compression
+    var scrollTop = 'scrollTop';
+    var scrollLeft = 'scrollLeft';
+
+    // returns scrollTop if argument is given, scrollLeft otherwise
+    var genScroll = function(top) {
+        var prop = top ? scrollTop : scrollLeft;
+        var dirAnimations = allAnimations[top?0:1];
+
+        // exported method
+        return function(elem, target, time) {
+            elem = elem.scroller || elem;  // needed for intence
+            time = time || 600;
+
+            var animation, tick, i = 0,
+                f0 = elem[prop], f1 = 0, f2 = 0;
+
+            // searching for the element's animation
+            for (;i < dirAnimations.length; i++) {
+                animation = (dirAnimations[i].e == elem) ?
+                    dirAnimations[i] : animation;
+            }
+
+            if (animation) {
+                f1 = animation.f[animation.n][1];
+                f2 = animation.f[animation.n][2];
+            } else {
+                // generating a new animation
+
+                // animation contains the following data:
+                // .e - element on which the animation is performed
+                // .f - animation frames
+                // .n - current frame number
+                // .r - timeout function rendering a single frame
+                // .t - starting timestamp
+                dirAnimations.push(animation = {e : elem});
+            }
+
+            animation.n = 0;
+            animation.t = (new Date).getTime();
+
+            // 20 msec is the delay between the frames
+            var fnum = animation.l = Math.floor(time/10);
+            var fnum2 = fnum * fnum;
+            var fnum3 = fnum2 * fnum;
+            var f0_target = f0-target;
+
+            // calculating initial frame
+            animation.f = [[
+                f0,  // first element is the value
+                f1,
+                f2,
+
+                // these magic formulae came from outer space
+                - ( 9 * f2 * fnum2 +
+                    (36 * f1 -9 * f2) * fnum -
+                    36 * f1 +
+                    60 * f0_target
+                ) / (fnum3 - fnum),
+
+                6 * ( 6 * f2 * fnum2 +
+                      (32 * f1 -6 * f2) * fnum -
+                      32 * f1 +
+                      60 * f0_target
+                ) / fnum / ( fnum3 + 2 * fnum2 - fnum - 2 ),
+                    
+                - 60 * ( f2 * fnum2 +
+                         (6 * f1 - f2) * fnum -
+                         6 * f1 +
+                         12 * f0_target
+                ) / fnum / (
+                    fnum2*fnum2  + 5*(fnum3 + fnum2-fnum) - 6 
+                )
+            ]];
+
+            // generating remaining frames
+            while(fnum--) {
+                var lastFrame = animation.f[animation.f.length-1];
+                var frame = [0,0,0,0,0,lastFrame[5]];
+
+                for (i = 4; i >= 0; i--) {
+                    frame[i] = lastFrame[i]+frame[i+1];
+                }
+
+                animation.f.push(frame);
+            }
+
+            if (!animation.r) {
+                var renderer;
+
+                // creating the function for the interval,
+                // and invoking it to apply the first frame instantly
+                (animation.r = renderer = function() {
+                     var timestamp = (new Date).getTime();
+                     animation.n = Math.min(
+                         Math.floor((timestamp - animation.t)/10),
+                         animation.f.length-1
+                     );
+
+                     var val = elem[prop] = animation.f[animation.n][0];
+
+                     if (animation.n < animation.f.length-1) {
+                         setTimeout(renderer, 10);
+                     } else {
+                         animation.r = animation.n = 0;
+                         animation.f = [[val, 0, 0, 0, 0, 0]];
+                     }
+                })();
+
+            }  // else animation is already running
+        }
+    }
+
+
+    exports[scrollTop] = genScroll(
+        exports[scrollLeft] = genScroll()
+    );
+}));
+
