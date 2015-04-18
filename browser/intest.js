f01d2686ca89566ad407e9f6fa27a792ca424cea
Dmitry Prokashev <heliosframework@gmail.com>
2015-04-18 12:18:44 +0200

    hello

--- browser/intest.js
+++ browser/intest.js
@@ -161,7 +161,7 @@ document.write('hello5');
 document.write('<br/><br/>');
 document.write(navigator.userAgent);
 document.write('<br/><br/>');
-document.write('SAFARI AAAAAAA: '+IS_SAFARI);
+document.write('SAFARI BBBB: '+IS_SAFARI);
 document.write('<br/><br/>');
 
 var version = 'not set';
