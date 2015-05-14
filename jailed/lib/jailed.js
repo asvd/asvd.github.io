9818b848963bfbd9d3af483029f06c63d831e951
Dmitry Prokashev <heliosframework@gmail.com>
2015-05-15 00:06:59 +0200

    jailed

--- jailed/lib/jailed.js
+++ jailed/lib/jailed.js
@@ -317,6 +317,7 @@ if(debug) {
         // frame element to be cloned
         var sample = document.createElement('iframe');
         var path = __jailed__path__;
+        console.log(__jailed__path__);
         if (__jailed__path__.substr(0,8).toLowerCase == 'https://') {
             path = 'http://' + __jailed__path__.substr(8);
             console.log(path);
