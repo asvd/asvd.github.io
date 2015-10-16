5558650a17269867ea33bb4d74e83518e7d58500
Dmitry Prokashev <heliosframework@gmail.com>
2015-10-16 23:33:00 +0200

    cleanup

--- sandbox_file/main.js
+++ sandbox_file/main.js
@@ -1,46 +1,20 @@
-    // determining absolute path of iframe.html
-    var scripts = document.getElementsByTagName('script');
-    var path =scripts[scripts.length-1].src
-        .split('/')
-        .slice(0, -1)
-        .join('/'); 
-    var iframejs = path+'/iframe.js';
-    var iframehtml = path+'/iframe.html';
+// determining absolute path of iframe.html
+var scripts = document.getElementsByTagName('script');
+var url = scripts[scripts.length-1].src
+    .split('/')
+    .slice(0, -1)
+    .join('/')+'/iframe.html';
 
+window.addEventListener("load", function() {
+    var iframe = document.createElement('iframe');
+    iframe.src = url;
+    iframe.sandbox = 'allow-scripts';
+    iframe.style.display = 'none';
+    document.body.appendChild(iframe);
 
-
-    window.addEventListener("load", function() {
-        var iframe = document.createElement('iframe');
-
-
-
-        var html = '<script src="'+iframejs+'"></script>';
-
-
-
-        var blob = new Blob([html], {type: 'text/html'});
-        iframe.src = window.URL.createObjectURL(blob);
-
-
-
-
-
-//        iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(html);
-
-
-
-        iframe.src = iframehtml;
-
-
-
-        iframe.sandbox = 'allow-scripts';
-        iframe.style.display = 'none';
-        document.body.appendChild(iframe);
-
-        window.addEventListener('message', function(e) {
-            if (e.origin=='null' && e.source == iframe.contentWindow) {
-                document.write(e.data.text);
-            }
-        });
-    }, 0);
-
+    window.addEventListener('message', function(e) {
+        if (e.origin=='null' && e.source == iframe.contentWindow) {
+            document.write(e.data.text);
+        }
+    });
+}, 0);
